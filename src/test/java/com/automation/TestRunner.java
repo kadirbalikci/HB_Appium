50a286e7e24d3cb9f1cf83d1042de82c42af1dde
Kadir Balikci <kadir.balikci@leanscale.com>
2020-12-29 23:27:34 +0300

    m

--- src/test/java/com/automation/TestRunner.java
+++ src/test/java/com/automation/TestRunner.java
@@ -19,9 +19,9 @@ public class TestRunner {
         try {
             DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
             desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-            desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "7.0");
-            desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"\\etsy.apk");
-            desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+            desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "10.0");
+            desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"\\hepsiburada_4.6.2.apk");
+            desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3a");
             desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
             driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
             Thread.sleep(3000);
--- src/test/java/com/automation/tests/day1/CalculatorTests.java
+++ src/test/java/com/automation/tests/day1/CalculatorTests.java
@@ -1,5 +1,6 @@
 package com.automation.tests.day1;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -13,21 +14,26 @@ import java.net.URL;
 
 public class CalculatorTests {
 
+    private AppiumDriver<MobileElement> driver;
+
     @Test
     public void test1() throws Exception {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-        desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "7.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "10.0");
         //desiredCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"\\etsy.apk");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3a");
         desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 20000);
 
-        desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
-        desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+        desiredCapabilities.setCapability("appActivity", "com.hepsiburada.ui.home.BottomNavigationActivity");
+        desiredCapabilities.setCapability("appPackage", "com.pozitron.hepsiburada");
 
-        AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+        driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
         Thread.sleep(3000);
         driver.closeApp();
+
+      //  AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+
 
     }
 
