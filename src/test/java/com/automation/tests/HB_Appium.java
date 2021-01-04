f0cffb3e11fbc5a1bd2be546df8bbe1036e74024
Kadir Balikci <kadir.balikci@leanscale.com>
2021-01-04 15:31:38 +0300

    m

--- src/test/java/com/automation/tests/HB_Appium.java
+++ src/test/java/com/automation/tests/HB_Appium.java
@@ -1,8 +1,10 @@
 package com.automation.tests;
 
+import com.github.javafaker.Faker;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
@@ -32,16 +34,17 @@ public class HB_Appium {
         driver = new AppiumDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
         Thread.sleep(3000);
 
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        Faker faker = new Faker();
 
+        WebDriverWait wait = new WebDriverWait(driver, 20);
 
         WebElement profile = driver.findElement(By.id("com.pozitron.hepsiburada:id/account_icon"));
         profile.click();
 
+        Thread.sleep(3000);
+
         WebElement Login = driver.findElementById("com.pozitron.hepsiburada:id/llUserAccountLogin");
 
-
-        //WebElement plus = driver.findElement(MobileBy.AccessibilityId("plus"));
         Login.click();
         Thread.sleep(3000);
         WebElement email = driver.findElementById("com.pozitron.hepsiburada:id/etLoginEmail");
@@ -51,24 +54,32 @@ public class HB_Appium {
         password.sendKeys("987D412w");
         LgnBtn.click();
 
+        Thread.sleep(3000);
         WebElement okBtn = driver.findElementById("android:id/button1");
 
         okBtn.click();
-
         Thread.sleep(3000);
-//      | HBV00000KKJ56 | Xiaomi Mi Band 4 Akıllı Bileklik Siyah          |
+
+        WebElement CloseProfileBtn = driver.findElementById("com.pozitron.hepsiburada:id/btnUserAccountClose");
+
+        CloseProfileBtn.click();
 
 
 
 
+        //Kategoriler Menu
+        Thread.sleep(3000);
+        WebElement categories = driver.findElementByAccessibilityId("navigationItem_1");
+        categories.click();
+        Thread.sleep(3000);
+        WebElement CatTitle = driver.findElementById("com.pozitron.hepsiburada:id/title");
+        String Actualtitle = CatTitle.getText();
+        String ExpectedTitle = "Kategoriler";
+        Assert.assertEquals(ExpectedTitle, Actualtitle);
 
 
 
-        //driver.closeApp();
-
-      //  AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-
+        driver.closeApp();
 
     }
-
 }
